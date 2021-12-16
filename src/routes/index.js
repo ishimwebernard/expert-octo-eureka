3f1e8662689620f21682b4a6bce2952c98b5830e
BERNARD ISHIMWE <isbernard2001@gmail.com>
2021-12-16 12:58:05 +0200

    mend

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import Application from "./api/applications"
 const router = Router();
+
+router.use('/application', Application)
 router.use('/', (req, res)=>{
     res.status(200).send({body: "Welcome to PRIMECS"})
 })
-router.use('/application', Application)
 export default router;
