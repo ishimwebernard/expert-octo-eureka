3aec398e1ea163d254716d0f6451da398afd2dda
BERNARD ISHIMWE <isbernard2001@gmail.com>
2021-12-16 12:22:10 +0200

    Adding welcome message

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
 import Application from "./api/applications"
 const router = Router();
+router.use('/', (req, res)=>{
+    res.status(200).send({body: "Welcome to PRIMECS"})
+})
 router.use('/application', Application)
 export default router;
