c0d4b0a512cbd237dc2247545491d7081640f1ac
BERNARD ISHIMWE <isbernard2001@gmail.com>
2021-12-16 12:55:05 +0200

    Adding a custom port for heroku

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app  = Express();
 app.use(bodyParser.json())
 app.use(cors());
 app.use("/", routes)
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log('Server Started');
 })
 export default app;
